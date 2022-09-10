4f108115b4ef00473b733868d65c7552a05b65c9
SoleEsp <soledad.espiritu909@gmail.com>
2022-09-10 00:20:36 -0300

    first commit

---
+++ 09 - Entrada-Salida y Manejo de Archivos/clase09_ej01.py
@@ -0,0 +1,11 @@
+import sys
+
+# 1) Crear un script con el nombre "clase09_ej1.py" que reciba 3 parametros a elección, verificando que sean exactamente esa cantidad, y muestre como salida los parámetros recibidos
+
+if (len(sys.argv) == 3):
+    print("Primer parametro: ",sys.argv[1])
+    print("Segundo parametro: ",sys.argv[2])
+    print("Tercer parametro: ",sys.argv[3])
+else:
+    print("ERROR: Debe ingresar 3 parametros")
+    print('Ejemplo: 1 2 3')
